6c032304af1fa8218d1938feca80bc902f6fb3d1
Anže Pečar <ensmotko@gmail.com>
2015-04-23 11:23:43 +0200

    Refactor fetch.go

--- gofetch/src/gofetch/fetch.go
+++ gofetch/src/gofetch/fetch.go
@@ -1,36 +1,18 @@
 package gofetch
 
 import (
-	"encoding/xml"
+	"io/ioutil"
+	"net/http"
 )
 
-type Forecast struct {
-	LastUpdate string `xml:"meta>lastupdate"`
-	NextUpdate string `xml:"meta>nextupdate"`
-	Sun        struct {
-		Rise string `xml:"rise,attr"`
-		Set  string `xml:"set,attr"`
-	} `xml:"sun"`
-}
+const (
+	URL = "http://www.yr.no/place/Croatia/Istria/Medulin/forecast.xml"
+)
 
-func ParseForecast(data string) Forecast {
-	f := Forecast{}
-	err := xml.Unmarshal([]byte(data), &f)
+func Fetch() string {
+	res, err := http.Get(URL)
 	panicErr(err)
-	return f
-}
-
-func Run() {
-	db := Db()
-	defer db.Close()
-
-	str := "INSERT INTO readings (lid, last_update, next_update, sun_rise, sun_set) values ($1, $2, $3, $4, $5);"
-	stmt, err := db.Prepare(str)
-	defer stmt.Close()
+	data, err := ioutil.ReadAll(res.Body)
 	panicErr(err)
-
-	_, err = stmt.Exec(1, "2015-04-18T09:18:16", "2015-04-18T09:18:17", "2015-04-18T09:18:18", "2015-04-18T09:18:19")
-	panicErr(err)
-
-	print("Done.")
+	return string(data)
 }
