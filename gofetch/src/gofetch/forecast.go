188d91fe61de52f54086c9e2ec70e40c64053f9c
Anže Pečar <ensmotko@gmail.com>
2015-04-25 23:20:42 +0200

    Add forecasts table
    We are now actually storing the data

---
+++ gofetch/src/gofetch/forecast.go
@@ -0,0 +1,68 @@
+package gofetch
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type Forecast struct {
+	gorm.Model
+	UpdateId          int `sql:"index"`
+	From              time.Time
+	To                time.Time
+	WindDirection     float32
+	WindDirectionName string
+	WindSpeed         float32
+	WindSpeedName     string
+	Temperature       float32
+	TemperatureName   string
+	Pressure          float32
+	PressureName      string
+	Symbol            float32
+	SymbolName        string
+}
+
+// TODO: The code below is just boilerplate because I don't know how
+//       to access ie <winddirection deg=""> deg without a winddirection
+//       struct. I also don't know how to correctly parse time.Time
+type ForecastXml struct {
+	From          string `xml:"from,attr"`
+	To            string `xml:"to,attr"`
+	WindDirection struct {
+		Deg  float32 `xml:"deg,attr"`
+		Code string  `xml:"code,attr"`
+	} `xml:"windDirection"`
+	WindSpeed struct {
+		Mps  float32 `xml:"mps,attr"`
+		Name string  `xml:"name,attr"`
+	} `xml:"windSpeed"`
+	Temperature struct {
+		Value float32 `xml:"value,attr"`
+		Name  string  `xml:"unit,attr"`
+	} `xml:"temperature"`
+	Pressure struct {
+		Value float32 `xml:"value,attr"`
+		Name  string  `xml:"unit,attr"`
+	} `xml:"pressure"`
+	Symbol struct {
+		Number float32 `xml:"number,attr"`
+		Name   string  `xml:"name,attr"`
+	} `xml:"symbol"`
+}
+
+func (f *ForecastXml) ToForecast() Forecast {
+	return Forecast{
+		From:              parse(f.From),
+		To:                parse(f.To),
+		WindDirection:     f.WindDirection.Deg,
+		WindDirectionName: f.WindDirection.Code,
+		WindSpeed:         f.WindSpeed.Mps,
+		WindSpeedName:     f.WindSpeed.Name,
+		Temperature:       f.Temperature.Value,
+		TemperatureName:   f.Temperature.Name,
+		Pressure:          f.Pressure.Value,
+		PressureName:      f.Pressure.Name,
+		Symbol:            f.Symbol.Number,
+		SymbolName:        f.Symbol.Name,
+	}
+}
