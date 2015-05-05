0565bbc47556e7a123d483589adb9b26520823bc
Anže Pečar <ensmotko@gmail.com>
2015-05-06 01:11:55 +0200

    Add bootstrap & font awesome

--- nginx/static/components/wavewatcher.ts
+++ nginx/static/components/wavewatcher.ts
@@ -1,15 +1,20 @@
-import {Component, bootstrap, View} from "angular2/angular2";
+import {Component, bootstrap, View, For, If} from "angular2/angular2";
 
 @Component({
     selector: 'wave-watcher'
 })
 @View({
-    templateUrl: "components/wavewatcher.template"
+    templateUrl: "components/wavewatcher.template",
+    directives: [For, If]
 })
 export class WaveWatcher {
-    name: string;
+    loading: boolean;
 
     constructor() {
-      this.name = 'Alice';
+      this.loading = true;
+      // Emulate loading data
+      setTimeout(function(){
+        this.loading = false;
+      }.bind(this), 1500)
     }
 }
